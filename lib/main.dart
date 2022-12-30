f27135c26e8bf94a018d966fa6096e1263a1a73c
yk-fjsk <77733479+yuki-fujisaki@users.noreply.github.com>
2022-12-30 15:34:10 +0900

    Revert "Feature/#001 connect GitHub api"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:github_repo_search/github_repo_search_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +14,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const GithubRepoSearchScreen(title: 'Github Repo Search'),
+      home: const MyHomePage(title: 'Github Repo Search'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(),
     );
   }
 }
