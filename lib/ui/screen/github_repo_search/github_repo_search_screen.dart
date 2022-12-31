82dc0afa9eade055216ae483f7d1da2a647d754b
yuki-fujisaki <yuki.f.bizz@gmail.com>
2022-12-31 17:09:46 +0900

    add: screenに適用

--- lib/ui/screen/github_repo_search/github_repo_search_screen.dart
+++ lib/ui/screen/github_repo_search/github_repo_search_screen.dart
@@ -1,40 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_repo_search/api/github_repository/github_repository.dart';
 import 'package:github_repo_search/ui/component/github_repo_list.dart';
 import 'package:github_repo_search/model/github_response/github_response.dart';
 import 'package:github_repo_search/ui/component/search_text_form.dart';
+import 'package:github_repo_search/ui/screen/github_repo_search/notifier/github_repo_search_screen_notifier.dart';
+import 'package:github_repo_search/ui/screen/github_repo_search/state/github_repo_search_screen_state.dart';
 
-class GithubRepoSearchScreen extends StatefulWidget {
+class GithubRepoSearchScreen extends ConsumerWidget {
   const GithubRepoSearchScreen({Key? key, required this.title})
       : super(key: key);
-
   final String title;
 
   @override
-  State<GithubRepoSearchScreen> createState() => _GithubRepoSearchScreenState();
-}
-
-class _GithubRepoSearchScreenState extends State<GithubRepoSearchScreen> {
-  GithubResponse? _response;
-
-  void load(String text) {
-    GithubRepository().fetch(text).then((response) {
-      setState(() {
-        _response = response;
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Stack(
         children: [
           Column(
-            children: [SearchTextForm(load), GithubRepoList(_response)],
+            children: [
+              SearchTextForm(
+                (text) => ref.read(gitreposProvider.notifier).load(text),
+              ),
+              GithubRepoList(ref.watch(githubRepoScreenStateProvider).response)
+            ],
           ),
         ],
       ),
